58ef7b486fb0c5459f787f1932f40f71cbf81cae
Varun2205 <varun.varma2205@gmail.com>
2026-02-21 16:47:24 +0530

    New change

--- LAB1/q4.tcl
+++
@@ -1,6 +0,0 @@
-set modules {alu regfile controller}
-set module_area {120 300 180}
-
-for {set i 0} {$i < [llength $modules]} {incr i} {
-    puts "[lindex $modules $i] : [lindex $module_area $i]"
-}
--- LAB1/q7.tcl
+++
@@ -1,7 +0,0 @@
-set modules {alu regfile controller fifo uart}
-foreach mod $modules {
-    puts "read_verilog ./rtl/$mod.v"
-}
-puts "All RTL files read Successfully"
-
-
